708acb9400ed255312c6c459842bc887384cdf6a
Laura Fontolan <laura_fontolan@hotmail.com>
2022-08-06 22:29:18 -0300

    create fruit list request

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,24 @@
+import { Button, Typography } from "@mui/material"
+import { useContext, useEffect } from "react"
 import { Header } from "../../components/Header"
+import { AppContext } from "../../context/app"
 import { Container } from "./styles"
 
 export const Home = () => {
+    const { getAllFruits, list} = useContext(AppContext)
+
+    useEffect(() => {
+        getAllFruits()
+    }, [])
+
     return (
         <Container>
             <Header />
+
+            {list.map(item => (
+                <Typography key={item.name}>{item?.name}</Typography>
+            ))}
+            
         </Container>
     )
 }

0c9a674a7e9c1a7316b8d1e131dfc89372613c75
Laura Fontolan <laura_fontolan@hotmail.com>
2022-08-07 23:20:00 -0300

    add background and card icon style

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,10 +3,13 @@ import { IconButton, TextField } from '@mui/material'
 import { SearchOutlined, ShoppingCartOutlined } from '@mui/icons-material'
 
 export const HeaderComponent = styled.header`
-    padding: 1.5rem 2.5rem;
-`
+    padding: 1rem 2.5rem;
+    background: var(--white);
 
-export const Logo = styled.h1``
+    a {
+        text-decoration: none;
+    }
+`
 
 export const Input = styled(TextField)`
     width: 500px;
@@ -20,4 +23,18 @@ export const Icon = styled(ShoppingCartOutlined)`
 
 export const SearchIcon = styled(SearchOutlined)`
     color: var(--gray-50);
+`
+
+export const CartArea = styled.section`
+    display: flex;
+    align-items: center;
+
+    .cart-length {
+        color: var(--gray-50);
+        font-size: 1.25rem;
+        margin-left: 0.25rem;
+
+        font-weight: 500;
+        font-family: 'Poppins', sans-serif;
+    }
 `

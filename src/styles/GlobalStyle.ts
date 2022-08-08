248ca0503bbeef22ff99655f32549641a8f38f5d
Laura Fontolan <laura_fontolan@hotmail.com>
2022-08-08 19:41:15 -0300

    change scrollbar style

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -6,6 +6,20 @@ export const GlobalStyle = createGlobalStyle`
         background: var(--gray-100);
     }
 
+    body::-webkit-scrollbar {
+        width: 12px;               
+    }
+
+    body::-webkit-scrollbar-track {
+        background: var(--gray-100);
+        box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+    }
+
+    body::-webkit-scrollbar-thumb {
+        background-color: var(--gray-50); 
+        border-radius: 20px;       
+    }
+
     :root {
         --gray-50: #939393;
         --gray-100: #f5f5f5;

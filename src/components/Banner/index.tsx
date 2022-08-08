8ef3447570a8583275f55d22569ef9a9d09bdc02
Laura Fontolan <laura_fontolan@hotmail.com>
2022-08-07 23:15:09 -0300

    create banner

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,11 @@
+import fruitsImage from '../../assets/images/fruits.png';
+
+export const Banner = () => {
+    return(
+        <img 
+            src={fruitsImage} 
+            alt='fruits'
+            height={150} 
+        />
+    )
+}
